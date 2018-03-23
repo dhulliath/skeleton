fa9b8b77b7759e284dd540c4905abe645db7e109
Peter Olejniczak <dhulliath@gmail.com>
2018-03-22 21:05:35 -0600

    more assets

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: '960697be6bc6a274cc3e7def96bf1496',
+			hash: 'f42a72bae4a9232928ddefd29d029a669e6ac9d9128505880e092cb411107817',
+			user_created_timestamp: 1521771856260
+		}
+	]
+})
